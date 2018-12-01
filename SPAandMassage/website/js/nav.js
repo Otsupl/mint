4e29bc5b3705758bdd9b677ce11bcf18dc1d5628
d3lusion <d3lusion@post.cz>
2018-12-01 14:46:55 +0100

    Dodano animacje swipe

--- SPAandMassage/website/js/nav.js
+++ SPAandMassage/website/js/nav.js
@@ -8,10 +8,14 @@ document.getElementById("clicker").addEventListener("click", event => {
   });
 function openNav() {
     document.getElementById("Sidenav").style.height = "100%";
+    document.body.style.overflow = "hidden";
     opened=true;
 }
 function closeNav() {
     document.getElementById("Sidenav").style.height = "0";
+    setTimeout(() => {
+      document.body.style.overflow = "scroll";
+    }, 200);
     opened=false;
 }
 $('#clicker').click(function(){
@@ -19,3 +23,11 @@ $('#clicker').click(function(){
       .toggleClass('menu')
       .toggleClass('close');
   })
+var swipeup = new Swipe('#swipeup');
+swipeup.onUp(function() { 
+    closeNav();
+    $('.icon')
+      .toggleClass('menu')
+      .toggleClass('close');
+ });
+swipeup.run();
