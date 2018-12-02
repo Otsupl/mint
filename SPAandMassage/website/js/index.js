6082dde5ca90bf66e0a616cd33d73dcef622e73a
d3lusion <d3lusion@post.cz>
2018-12-02 12:59:00 +0100

    Poprawiono czas zanikania

--- SPAandMassage/website/js/index.js
+++ SPAandMassage/website/js/index.js
@@ -1,4 +1,4 @@
 $(window).scroll(function(){
-    $("#welcometxt").css("opacity", 1 - $(window).scrollTop() / 300);
-    $("#discover").css("opacity", 1 - $(window).scrollTop() / 400);
+    $("#welcometxt").css("opacity", 1 - $(window).scrollTop() / 90);
+    $("#discover").css("opacity", 1 - $(window).scrollTop() / 435);
   });
