08b0d1d9454cc629c80e72fd111bf853d71c6544
d3lusion <d3lusion@post.cz>
2018-12-12 22:51:01 +0100

    Dodano nowy nav, zmieniono kolory

--- SPAandMassage/website/js/index.js
+++ SPAandMassage/website/js/index.js
@@ -1,5 +1,5 @@
 $(window).scroll(function(){
-    $("#welcometxt").css("opacity", 1 - $(window).scrollTop() / 90);
+    $("#indexwelcometxt").css("opacity", 1 - $(window).scrollTop() / 400);
     $("#discover").css("opacity", 1 - $(window).scrollTop() / 435);
     if(window.matchMedia("(min-width: 992px)").matches)
     {
