01a16de0d3b5e7769bacb69b803f35cd93615af9
Radosław <radziowojak@gmail.com>
2018-12-27 16:06:19 +0100

    przerobiono nava

--- SPAandMassage/website/js/nav.js
+++ SPAandMassage/website/js/nav.js
@@ -1,37 +1,14 @@
 var opened = false;
 $(window).scroll(function() {
-  if ($(this).scrollTop() > 80) { 
-      $(".navbar").css({height: '50px',background: 'rgba(255, 255, 255, 0.833)'});
-      $(".logo").css({padding: '10px 0 10px  0'});
-      $(".logo img").css({width: '70px'});
-      $(".outerburger").css({top: '23px'});
-  }
+  if ($(this).scrollTop() > 80)
+    document.getElementById("navbar").classList.add('nav-small');
   else
-  {
-    $(".navbar").css({height: '80px',background: 'rgba(255, 255, 255, 0.342)'});
-      $(".logo").css({padding: '20px 0 20px  0'});
-      $(".logo img").css({width: '100px'});
-      $(".outerburger").css({top: '40px'});
-  }
+    document.getElementById("navbar").classList.remove('nav-small');
 });
-function open()
-{
-  $('#sidenav').css({width: '200px'});
-  $('#sidenav ul').css({opacity: '1'});
-  $('.close.icon').css({opacity: '1'});
-  $('body').css({overflow: 'hidden'});
-}
-function close()
-{
-  $('#sidenav').css({width: '0px'});
-  $('#sidenav ul').css({opacity: '0'});
-  $('.close.icon').css({opacity: '0'});
-  $('body').css({overflow: 'scroll'});
-}
 
-$( "#clicker" ).click(function() {
-  open();
-});
-$( "#outerclose" ).click(function() {
-  close();
+document.getElementById('burgeract').addEventListener('click',function()
+{
+    document.getElementById("menu").classList.toggle('nav-open');
+	document.getElementById('burger').classList.toggle('menu');
+	document.getElementById('burger').classList.toggle('close');
 });
