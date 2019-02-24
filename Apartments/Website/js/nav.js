82dca2cad8e9130c43753c9216af55c8716eaa97
d3lusion <d3lusion@post.cz>
2019-02-24 01:06:16 +0100

    Rozpoczęto tworzenie nowej sekcji, poprawki kosmetyczne, poprawki position na divie bgcontainer

---
+++ Apartments/Website/js/nav.js
@@ -0,0 +1,20 @@
+/*
+$(document).ready(function(){
+    var scrollTop = 0;
+    $(window).scroll(function(){
+      scrollTop = $(window).scrollTop();
+      
+      if (scrollTop >= 10) {
+          $('#navcontainer ul span').fadeOut();
+          $('#navcontainer').addClass('scrolled');
+        
+      } 
+      else if (scrollTop < 10) {
+        $('#navcontainer ul span').fadeIn();
+        $('#navcontainer').removeClass('scrolled');
+      } 
+      
+    }); 
+    
+  });
+  */
