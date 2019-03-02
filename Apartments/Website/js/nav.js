d532bf71e0b0511c929a105850884ec104711408
Radosław <radziowojak@gmail.com>
2019-03-02 23:02:16 +0100

    dodano zmianę aktywnych kropek przy scrollowaniu

--- Apartments/Website/js/nav.js
+++ Apartments/Website/js/nav.js
@@ -4,7 +4,7 @@ $(document).ready(function()
   $(window).scroll(function()
   {
     scrollTop = $(window).scrollTop();
-
+    
     if (scrollTop >= 10) 
     {
       $('#navcontainer ul span').fadeOut(600);
@@ -15,5 +15,18 @@ $(document).ready(function()
       $('#navcontainer ul span').fadeIn(800);
       $('#navcontainer').removeClass('nav-container-scrolled');
     } 
+
+    /*ifami najszybciej*/
+    var vhx50 = 0.5*$(window).height();
+    if(scrollTop < $('#about').offset().top - vhx50)
+      {$('.active').removeClass('active'); $('#dot_home').addClass('active');}
+    else if(scrollTop < $('#whyus').offset().top - vhx50)
+      {$('.active').removeClass('active'); $('#dot_about').addClass('active');}
+    else if(scrollTop < $('#photos').offset().top - vhx50)
+      {$('.active').removeClass('active'); $('#dot_whyus').addClass('active');}
+    else if(scrollTop < $('footer').offset().top - vhx50)
+      {$('.active').removeClass('active'); $('#dot_photos').addClass('active');}
+    else
+      {$('.active').removeClass('active'); $('#dot_contact').addClass('active');}
   }); 
 });
