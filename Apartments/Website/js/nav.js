62811d0086f6ec513c60c520a248e687dd7ce388
Radosław Kamiński <radziowojak@gmail.com>
2019-03-17 22:17:40 +0100

    hover z nava usuwa się na każdym urządzeniu

--- Apartments/Website/js/nav.js
+++ Apartments/Website/js/nav.js
@@ -21,24 +21,11 @@ $(document).ready(function()
     $('html, body').animate({
       scrollTop: $("#"+sections[event.data.id]).offset().top - 100
     }, 2000);
-    if(/*$(window).width() < 990 ||*/ is_touch_device())setTimeout(function(){$("#navcontainer li").css({'pointer-events': 'none'})}, 2000);
+    setTimeout(function(){$("#navcontainer li").css({'pointer-events': 'none'})}, 2000);
+    setTimeout(function(){$("#navcontainer li").css({'pointer-events': ''})}, 2100);
+
   }
 });
-function is_touch_device() {
-  var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-  var mq = function(query) {
-    return window.matchMedia(query).matches;
-  }
-
-  if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-    return true;
-  }
-
-  // include the 'heartz' as a way to have a non matching MQ to help terminate the join
-  // https://git.io/vznFH
-  var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
-  return mq(query);
-}
 function onScrollNav()
 {
   scrollTop = $(window).scrollTop();
