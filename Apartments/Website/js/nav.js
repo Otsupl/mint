df2cec187636e8d0275c235944444214944dd353
Radosław Kamiński <radziowojak@gmail.com>
2019-03-24 22:17:30 +0100

    zrobiono timeline

--- Apartments/Website/js/nav.js
+++ Apartments/Website/js/nav.js
@@ -63,13 +63,13 @@ function onScrollNav()
   /*ifami najszybciej*/
   var vhx50 = 0.5*vh;
   if(scrollTop < $('#about').offset().top - vhx50)
-    {$('.active').removeClass('active'); $('#dot_home').addClass('active');}
+    {$('#navcontainer .active').removeClass('active'); $('#dot_home').addClass('active');}
   else if(scrollTop < $('#whyus').offset().top - vhx50)
-    {$('.active').removeClass('active'); $('#dot_about').addClass('active');}
+    {$('#navcontainer .active').removeClass('active'); $('#dot_about').addClass('active');}
   else if(scrollTop < $('#photos').offset().top - vhx50)
-    {$('.active').removeClass('active'); $('#dot_whyus').addClass('active');}
+    {$('#navcontainer .active').removeClass('active'); $('#dot_whyus').addClass('active');}
   else if(scrollTop < $('#contact').offset().top - vhx50)
-    {$('.active').removeClass('active'); $('#dot_photos').addClass('active');}
+    {$('#navcontainer .active').removeClass('active'); $('#dot_photos').addClass('active');}
   else
-    {$('.active').removeClass('active'); $('#dot_contact').addClass('active');}
+    {$('#navcontainer .active').removeClass('active'); $('#dot_contact').addClass('active');}
 }
